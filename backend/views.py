6794a96c6da4eb53756ccbf1215aaf493c85c26c
Louis-He <1726110778@qq.com>
2021-06-23 22:17:51 +0800

    initialize the project

---
+++ backend/views.py
@@ -0,0 +1,23 @@
+from django.views.generic import View
+from django.conf import settings
+from django.http import HttpResponse
+import os
+
+
+# Create your views here.
+class FrontendAppView(View):
+    def get(self, request):
+        print(os.path.join(settings.REACT_APP_DIR, 'build', 'index.html'))
+        try:
+            with open(os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')) as f:
+                return HttpResponse(f.read())
+        except FileNotFoundError:
+            print('Production build of app not found')
+            return HttpResponse(
+                """
+                This URL is only used when you have built the production
+                version of the app. Visit http://localhost:3000/ instead, or
+                run `yarn run build` to test the production version.
+                """,
+                status=501,
+            )
