5b7b409c5906ec993cd7780146e148234d6833d8
Cathy Feng <fengyuchun2001@gmail.com>
2021-07-07 23:14:52 -0400

    stores input in the state; 20% viewport height

--- frontend/src/Components/SearchBar.js
+++ frontend/src/Components/SearchBar.js
@@ -1,22 +1,46 @@
 import React, {useState} from 'react';
 import MsgBox from "./MsgBox";
+import '../SearchBar.css';
 
 function SearchItem(props){
     const[item, setItem] = useState("");
 }
 
 class SearchBar extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            query: '',
+            height: props.height
+        }
+    }
+
+    handleOnInputChange = (event) => {
+        const query = event.target.value;
+        this.setState({query: query});
+    }
     render() {
+        const {query} = this.state; /* save query in state into constant query */
+        console.warn(this.state);
         return (
             <div className = "container">
+            <h2>Type something to search:</h2>
+            <label className="search-label" htmlFor="search-input">
+                <input
+                        type="text"
+                        name="query"
+                        value={query}
+                        id="search-input"
+                        className="input"
+                        placeholder="Search..."
+                        onChange={this.handleOnInputChange}
+
+                    />
+
+                <button type="button" className="btn btn-primary"> Search </button>
+            </label>
                 <div>
-                    <h2>
-                        Type something to search:
-                    </h2>
-                </div>
-                <div>
-                    <input type="text" className="input" placeholder="Search..." />
-                    <button type="button" className="btn btn-primary"> Search </button>
+                    <div>You just searched for: { query }</div>
                 </div>
 
             </div>
