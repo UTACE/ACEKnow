e60e7e2757fdb730fc19002d05c8fb84291001cd
Cathy Feng <fengyuchun2001@gmail.com>
2021-07-10 21:34:30 -0400

    hi

--- frontend/src/Components/HomePage/SearchBar.js
+++ frontend/src/Components/HomePage/SearchBar.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React from 'react';
+import {Container} from "react-bootstrap"
+import DevMsgBox from "./Components/HomePage/DevMsgBox";
+import MsgBox from "./Components/Misc/MsgBox";
 
-function SearchBar(){
-    return(
-        <div>
-            Hello
-        </div>
+import {mainDomain} from "./configuration";
+import SearchBar from "./Components/HomePage/SearchBar";
+
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      djangoDev: false,
+    }
+  }
+
+  componentDidMount() {
+    fetch(mainDomain + 'api/getDebugInfo/', {
+      method: 'GET',
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    }).then(response => {return response.json()})
+      .then(response => {
+        this.setState({
+          djangoDev: response.debug
+        })
+    })
+  }
+
+  render() {
+    return (
+      <Container>
+        <DevMsgBox djangoDev={this.state.djangoDev} reactDev={this.props.reactDev}/>
+        <MsgBox variant = "success" content = "You have created an alert."/>
+        <SearchBar/>
+      </Container>
     )
+  }
 }
-export default SearchBar;
+
+// ========================================
+export default HomePage;
