7332e926fe5d8a688b5350f7aa5f5b45a0228366
Robert Ren <robert.ren@mail.utoronto.ca>
2021-07-18 17:31:11 -0400

    added about me button and link to utace, collapse is not very smooth yet

--- frontend/src/Components/Misc/NavBar.js
+++ frontend/src/Components/Misc/NavBar.js
@@ -1,10 +1,13 @@
 import React, {Component} from "react";
 import '../../styles/NavBar.css'
 import 'bootstrap/dist/css/bootstrap.css'
-import { Nav, Navbar} from 'react-bootstrap'
+import { Nav, Navbar, NavDropdown} from 'react-bootstrap'
 
 class NavBar extends Component{
 
+    onClick(){
+        window.location.href="https://www.utace.club/";
+    }
     render(){
         return (
             <div className=" Navbar">
@@ -17,14 +20,37 @@ class NavBar extends Component{
                 <Navbar.Toggle className="coloring" />
                 <Navbar.Collapse>
                   <Nav>
-                    <Nav.Link href="#1" class = "nav-links">返加相关</Nav.Link>
-                    <Nav.Link href="#2" class = "nav-links">回国相关</Nav.Link>
-                    <Nav.Link href="#3" class = "nav-links">加拿大疫情情况</Nav.Link>
-                    <Nav.Link href="#4" class = "nav-links">航班线路</Nav.Link>
-                    <Nav.Link href="#5" class = "nav-links">其他</Nav.Link>
+                    <NavDropdown title="返加相关">
+                      <NavDropdown.Item href="#ca/1">1</NavDropdown.Item>
+                      <NavDropdown.Item href="#ca/2">2</NavDropdown.Item>
+                      <NavDropdown.Item href="#ca/3">3</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="回国相关">
+                      <NavDropdown.Item href="#cn/1">1</NavDropdown.Item>
+                      <NavDropdown.Item href="#cn/2">2</NavDropdown.Item>
+                      <NavDropdown.Item href="#cn/3">3</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="加拿大疫情情况">
+                      <NavDropdown.Item href="#covid/1">1</NavDropdown.Item>
+                      <NavDropdown.Item href="#covid/2">2</NavDropdown.Item>
+                      <NavDropdown.Item href="#covid/3">3</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="航班线路">
+                      <NavDropdown.Item href="#flight/1">1</NavDropdown.Item>
+                      <NavDropdown.Item href="#flight/2">2</NavDropdown.Item>
+                      <NavDropdown.Item href="#flight/3">3</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="其他">
+                      <NavDropdown.Item href="#others/1">1</NavDropdown.Item>
+                      <NavDropdown.Item href="#others/2">2</NavDropdown.Item>
+                      <NavDropdown.Item href="#others/3">3</NavDropdown.Item>
+                    </NavDropdown>
                   </Nav>
+                    <button class="ml-auto btn btn-outline-info" onClick={this.onClick}>关于我们</button>{' '}
                 </Navbar.Collapse>
+
               </Navbar>
+
             </div>
           );
     }
