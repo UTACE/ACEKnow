02c3309ea1dcc00e2a500ca05f80dfbe3c915140
CatherineZM <49298813+CatherineZM@users.noreply.github.com>
2021-08-03 13:38:57 -0400

    Added router for canada related pages

---
+++ frontend/src/Components/Canada/AfterArrival.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavBar from "../Misc/NavBar";
+import Footer from "../HomePage/Footer";
+
+class AfterArrival extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavBar/>
+        <div>Nothing for after arrival yet</div>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+// ========================================
+export default AfterArrival;
