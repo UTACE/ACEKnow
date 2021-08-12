5de31dc8a7a74d75776caeb3a7ea3ef801c900d9
Robert Ren <robert.ren@mail.utoronto.ca>
2021-08-12 17:21:23 -0400

    added switchhealth link

--- frontend/src/Components/Canada/BeforeArrival.js
+++ frontend/src/Components/Canada/BeforeArrival.js
@@ -81,10 +81,30 @@ class BeforeArrival extends React.Component {
                       Toronto
                     </Dropdown.Toggle>
                     <Dropdown.Menu>
-                      <Dropdown.Item>
-                        <b>Contact SwitchHealth by phone or email</b>
+                      <Dropdown.Item href="https://portal.switchhealth.ca/">
+                        <b>Contact <a className = 'a1-before' href="https://portal.switchhealth.ca/">SwitchHealth</a> by phone or email</b>
                       </Dropdown.Item>
                       <Dropdown.Divider></Dropdown.Divider>
+                      <Dropdown.Item>
+                        <p>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                               className="bi bi-telephone" viewBox="0 0 16 16">
+                            <path
+                                d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                          </svg>
+                          &nbsp; Toll-free: 1-888-966-6531 &nbsp;
+                        </p>
+                      </Dropdown.Item>
+                      <Dropdown.Item>
+                        <p>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                               className="bi bi-telephone" viewBox="0 0 16 16">
+                            <path
+                                d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                          </svg>
+                          &nbsp; Phone: 1-647-977-1030 &nbsp;
+                        </p>
+                      </Dropdown.Item>
                       <Dropdown.Item onClick={event => {copy("results@switchhealth.ca")}}>
                         <p>
                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -102,19 +122,8 @@ class BeforeArrival extends React.Component {
                           </svg>
                         </p>
                       </Dropdown.Item>
-                      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
                     </Dropdown.Menu>
                   </Dropdown>
-                  <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">Contact SwitchHealth by phone or email
-                    <p className="p2-before"></p>
-                    <ul className="ul-before">
-                      <li className="p2-before">Toll-free: 1-888-966-6531</li>
-                      <li className="p2-before">Phone: 1-647-977-1030</li>
-                      <li className="p2-before">Email: results@switchhealth.ca</li>
-                    </ul>
-                    <a class="dropdown-item" href="https://getbootstrap.com/docs/4.0/components/button-group/">Dropdown link</a>
-                    <a class="dropdown-item" href="https://getbootstrap.com/docs/4.0/components/button-group/">Dropdown link</a>
-                  </div>
                 </div>
               </div>
             </li>
