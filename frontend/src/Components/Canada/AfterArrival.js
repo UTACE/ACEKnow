1ef26fd74a221bfb048ea16c4484b5c9e498618a
Ethan-Haodong-Ma <86768021+Ethan-Haodong-Ma@users.noreply.github.com>
2021-08-12 18:05:03 +0800

    Update AfterArrival.js

--- frontend/src/Components/Canada/AfterArrival.js
+++ frontend/src/Components/Canada/AfterArrival.js
@@ -3,7 +3,7 @@ import NavBar from '../Misc/NavBar';
 import Footer from '../HomePage/Footer';
 import '../../styles/AfterArrival.css';
 
-class UponArrival extends React.Component {
+class AfterArrival extends React.Component {
   render() {
     return (
       <div>
