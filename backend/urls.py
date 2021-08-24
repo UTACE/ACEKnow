2ebb360008c3ed6ee9bb984f9610a41a44dc3f36
Louis-He <1726110778@qq.com>
2021-08-24 20:15:01 +0800

    add backend for covid map

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.conf import settings
-from .views import GetDebugInfoAPIView
+from .views import GetDebugInfoAPIView, GetCovidDataAPIView
 
 # url patterns
 urlpatterns = [
     path('getDebugInfo/', GetDebugInfoAPIView.as_view(), name='get_debug_info'),
+    path('getCovidData/', GetCovidDataAPIView.as_view(), name='get_covid_data')
 ]
