a285374ee109e5abeef105625b9bfb29b2c786ee
Louis-He <1726110778@qq.com>
2021-09-05 20:10:46 -0400

    update health code verification page

--- frontend/src/Components/HealthCode/VerifyHealthCodePage.js
+++ frontend/src/Components/HealthCode/VerifyHealthCodePage.js
@@ -4,29 +4,34 @@ import { Redirect } from 'react-router'
 
 import NavBar from "./../Misc/NavBar";
 import Footer from "./../HomePage/Footer";
-import {Container} from "react-bootstrap";
+import {Row, Col, Container, Button} from "react-bootstrap";
+import { CheckCircle, ExclamationCircle, XCircle, QuestionCircle } from 'react-bootstrap-icons';
 
 class VerifyHealthCodePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      result: 'No result'
+      scanned: false,
+      result: 'No result',
+      healthCodeRes: {color: "U"}
     }
     console.log(props.isLoggedIn)
   }
 
   handleScan = data => {
-    console.log("HANDLE SCAN", data)
     var that = this
     if (data) {
       this.setState({
+        scanned: true,
         result: data
       })
 
       this.props.requestHandler("GET", data)
         .then(response => response.json())
         .then(response => {
-          console.log(response)
+          that.setState({
+            healthCodeRes: response
+          })
       })
     }
   }
@@ -35,25 +40,72 @@ class VerifyHealthCodePage extends React.Component {
     console.error(err)
   }
 
+  scanNext = () => {
+    this.setState({
+      scanned: false
+    })
+  }
+
   render() {
     if (!this.props.isLoggedIn && this.props.isLoggedInChecked) {
       return <Redirect to='/login'/>;
     } else if (!this.props.isLoggedInChecked) {
       return <p>Authenticating...</p>
     }
+    let body
+
+    if (!this.state.scanned) {
+       body =
+         <Row>
+          <Col>
+            <QrReader
+              delay={100}
+              onError={this.handleError}
+              onScan={this.handleScan}
+              style={{ width: '100%', maxWidth: "600px",}}
+            />
+          </Col>
+         </Row>
+    } else {
+      let color, variant, message, detailedMessage
+      if (this.state.healthCodeRes.color === "G") {
+        color = "#00CC00"
+        variant = <CheckCircle style={{fontSize: "64px"}}/>
+        message = "Green 绿色"
+      } else if (this.state.healthCodeRes.color === "Y") {
+        color = "#FFCC00"
+        variant = <ExclamationCircle style={{fontSize: "64px"}}/>
+        message = "Yellow 黄色"
+      } else if (this.state.healthCodeRes.color === "R") {
+        color = "#FF0000"
+        variant = <XCircle style={{fontSize: "64px"}}/>
+        message = "Red 红色"
+      } else if (this.state.healthCodeRes.color === "U") {
+        color = "#555555"
+        variant = <QuestionCircle style={{fontSize: "64px"}}/>
+        message = "Gray 灰色"
+      }
+
+      body =
+        <div>
+          <p>Identified Link: {this.state.result}</p>
+          <div style={{color: color}}>
+            <Row><Col style={{textAlign: "center"}}>{variant}</Col></Row>
+            <Row><Col style={{textAlign: "center"}}><strong>{message}</strong></Col></Row>
+            <Row>
+              <Col style={{textAlign: "center"}}><strong>{this.state.healthCodeRes.lastName + ", " + this.state.healthCodeRes.firstName}</strong></Col>
+            </Row>
+            <Row><Col><Button onClick={this.scanNext}>Scan Next</Button></Col></Row>
+          </div>
+        </div>
+    }
+
     return (
       <div>
         <NavBar isLoggedIn={this.props.isLoggedIn} logoutHandler={this.props.logoutHandler}/>
         <Container>
-          <QrReader
-            delay={100}
-            onError={this.handleError}
-            onScan={this.handleScan}
-            style={{ width: '100%', maxWidth: "300px" }}
-          />
+          {body}
         </Container>
-
-        <p>{this.state.result}</p>
         <Footer/>
       </div>
     )
